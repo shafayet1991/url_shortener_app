66457645c47ebb04e353b17039b0cad2aad7a207
shafayet.hossen <shafayet.hossen@reddotdigitalit.com>
2022-07-03 20:45:35 +0600

    first commit

---
+++ database/migrations/2022_07_03_143012_create_urls_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUrlsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('urls', function (Blueprint $table) {
+            $table->id();
+            $table->mediumText('old_url');
+            $table->mediumText('new_url');
+            $table->mediumText('user_ip');
+            $table->integer('user_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('urls');
+    }
+}
